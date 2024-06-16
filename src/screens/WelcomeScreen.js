bdab34772daf5e763ed195d277ac691a7943749a
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2024-06-16 10:26:36 +0700

    finish

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const WelcomeScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+      <Text>WelcomeScreen\</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
